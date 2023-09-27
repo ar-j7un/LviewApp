4542a34169cca991b86cd06dd73216f4f22e14cb
CCLAB15 <CCLAB15@ccl15.rcss.com>
2023-09-27 10:45:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lviewapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.lviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity implements AdapterView.OnItemClickListener {
+    static final String []lst = new String[] {"One", "Two", "Three"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_main);
+
+
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_main,lst));
+        ListView listView =getListView();
+        listView.setTextFilterEnabled(true);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+
+        String s;
+
+        TextView tv = (TextView) arg1;
+        s= (String) tv.getText().toString();
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+
+    }
+}
